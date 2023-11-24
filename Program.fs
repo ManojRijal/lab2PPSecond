1ddd67dd90e3aff7d99cfda638994feab7eb47bb
Manoj Rijal <manojsmacbook@Manojs-MacBook-Air.local>
2023-11-24 11:08:30 -0500

    Final commit

--- Program.fs
+++ Program.fs
@@ -19,3 +19,23 @@ type Activity =
     | LongDrive of int * float
 
 
+let calculateBudget activity =
+    match activity with
+    | BoardGame | Chill -> 0.0
+    | Movie movieType ->
+        match movieType with
+        | Regular -> 12.0
+        | IMAX -> 17.0
+        | DBOX -> 20.0
+        | RegularWithSnacks -> 12.0 + 5.0
+        | IMAXWithSnacks -> 17.0 + 5.0
+        | DBOXWithSnacks -> 20.0 + 5.0
+    | Restaurant cuisine ->
+        match cuisine with
+        | Korean -> 70.0
+        | Turkish -> 65.0
+    | LongDrive (km, fuelPerKm) -> float km * fuelPerKm
+
+let Date = Movie IMAXWithSnacks 
+let budget = calculateBudget Date
+printfn "The total budget for date is  $%.2f" budget
