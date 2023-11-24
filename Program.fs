cd6adcb63bb2356736a3807fda26bdcca38aaece
Manoj Rijal <manojsmacbook@Manojs-MacBook-Air.local>
2023-11-24 11:07:42 -0500

    Second commit

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿type Cuisine =
+    | Korean
+    | Turkish
+
+type MovieType =
+    | Regular
+    | IMAX
+    | DBOX
+    | RegularWithSnacks
+    | IMAXWithSnacks
+    | DBOXWithSnacks
+
+
+type Activity =
+    | BoardGame
+    | Chill
+    | Movie of MovieType
+    | Restaurant of Cuisine
+    | LongDrive of int * float
+
+
